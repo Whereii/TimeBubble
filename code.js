0c1f84892784a42ab03e57a50d2bbc401f76e987
JoshuaMcCausey <joshuamccausey@gmail.com>
2023-05-30 22:08:30 -0400

    New pictures 05/30/23

--- code.js
+++ code.js
@@ -411,7 +411,7 @@ function bubbleGeneratorFour() {
 
 
 //Picks which image to use randomly for openMemory
-let selection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+let selection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 let alreadyPicked = []
 function memoryImage() {
   if(selection.length == 0){
